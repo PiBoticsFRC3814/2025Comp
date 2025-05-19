9035fff8693dd97aeb1debe554dc1d64528c721f
RBidleman <hambonebbq7@gmail.com>
2025-05-19 19:58:10 -0400

    i wanna fight this code sometimes - RB

--- 2025Comp/src/main/java/frc/robot/subsystems/SwerveModuleSimulationConfig.java
+++ 2025Comp/src/main/java/frc/robot/subsystems/SwerveModuleSimulationConfig.java
@@ -2,176 +2,46 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-
 package frc.robot.subsystems;
+import  frc.robot.Constants;
 
-import static edu.wpi.first.units.Units.*;
+import org.ironmaple.simulation.drivesims.COTS;
+import org.ironmaple.simulation.drivesims.configs.DriveTrainSimulationConfig;
+
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.measure.*;
-import java.util.function.Supplier;
-import org.ironmaple.simulation.SimulatedArena;
-import org.ironmaple.simulation.drivesims.SwerveModuleSimulation;
-import org.ironmaple.simulation.motorsims.SimMotorConfigs;
-import frc.robot.Constants;
 
+public class SwerveModuleSimulationConfig extends SubsystemBase {
+  public final DCMotor driveMotor;
+  public final DCMotor steerMotor;
+  public final double driveGearRatio;
+  public final double steerGearRatio;
+  public final double driveFrictionVoltage;
+  public final double steerFrictionVoltage;
+  public final double wheelRadius;
+  public final double steerMOI;
+  public final double wheelCOF;
 
+  /** Creates a new SwerveModuleSimulationConfig. */
+  public SwerveModuleSimulationConfig(DCMotor driveMotor, DCMotor steerMotor, double driveGearRatio, double steerGearRatio,
+                                      double driveFrictionVoltage, double steerFrictionVoltage, double wheelRadius,
+                                      double steerMOI, double wheelCOF) {
+    this.driveMotor = DCMotor.getNEO(1);
+    this.steerMotor = DCMotor.getNEO(1);
+    this.driveGearRatio = driveGearRatio; // get the rest at some point
+    this.steerGearRatio = steerGearRatio;
+    this.driveFrictionVoltage = driveFrictionVoltage;
+    this.steerFrictionVoltage = steerFrictionVoltage;
+    this.wheelRadius = wheelRadius;
+    this.steerMOI = steerMOI; // VERY difficult to calculate but is important. If RB not there ask DL for help
+    this.wheelCOF = wheelCOF; // same here :shrug:
+  }
+ 
 
-public class SwerveModuleSimulationConfig implements Supplier<SwerveModuleSimulation> {
-
-    public final SimMotorConfigs driveMotorConfigs, steerMotorConfigs;
-
-    public final double DRIVE_GEAR_RATIO, STEER_GEAR_RATIO, WHEELS_COEFFICIENT_OF_FRICTION;
-    public final Voltage DRIVE_FRICTION_VOLTAGE;
-    public final Distance WHEEL_RADIUS;
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
     
-    public static void check(double value, double min, double max, String parameterName, String unitDescription) {
-
-        if (value < min || value > max) {
-
-            throw new IllegalArgumentException(
-
-                String.format(
-
-                    "%s (%f %s) is out of bounds. Must be between %f and %f %s.",
-
-                    parameterName, value, unitDescription, min, max, unitDescription
-
-                )
-
-            );
-
-        }
-
-    }
-
-
-
-    public SwerveModuleSimulationConfig(
-
-            DCMotor driveMotorModel,
-
-            DCMotor steerMotorModel,
-
-            double driveGearRatio,
-
-            double steerGearRatio,
-
-            Voltage driveFrictionVoltage,
-
-            Voltage steerFrictionVoltage,
-
-            Distance wheelRadius,
-
-            MomentOfInertia steerRotationalInertia,
-
-            double wheelsCoefficientOfFriction) {
-
-        check(driveGearRatio, 4, 24, "drive gear ratio", "times reduction");
-
-        check(steerGearRatio, 6, 50, "steer gear ratio", "times reduction");
-
-        check(driveFrictionVoltage.in(Volts), 0.01, 0.35, "drive friction voltage", "volts");
-
-        check(steerFrictionVoltage.in(Volts), 0.01, 0.6, "steer friction voltage", "volts");
-
-        check(wheelRadius.in(Inches), 1, 3.2, "drive wheel radius", "inches");
-
-        check(
-
-                steerRotationalInertia.in(KilogramSquareMeters), 0.005, 0.06, "steer rotation inertia", "kg * m^2");
-
-        check(wheelsCoefficientOfFriction, 0.6, 1.9, "tire coefficient of friction", "");
-
-
-
-        this.driveMotorConfigs =
-
-                new SimMotorConfigs(driveMotorModel, driveGearRatio, KilogramSquareMeters.zero(), driveFrictionVoltage);
-
-        this.steerMotorConfigs =
-
-                new SimMotorConfigs(steerMotorModel, steerGearRatio, steerRotationalInertia, steerFrictionVoltage);
-
-        DRIVE_GEAR_RATIO = driveGearRatio;
-
-        STEER_GEAR_RATIO = steerGearRatio;
-
-        WHEELS_COEFFICIENT_OF_FRICTION = wheelsCoefficientOfFriction;
-
-        DRIVE_FRICTION_VOLTAGE = driveFrictionVoltage;
-
-        WHEEL_RADIUS = wheelRadius;
-
-    }
-
-    
-
-    @Override
-
-    public SwerveModuleSimulation get() {
-
-        return new SwerveModuleSimulation(
-            driveMotorConfigs, 
-            steerMotorConfigs, 
-            DRIVE_GEAR_RATIO, 
-            STEER_GEAR_RATIO, 
-            WHEEL_RADIUS, 
-            WHEELS_COEFFICIENT_OF_FRICTION
-        );
-        
-
-    }
-
-
-
-    public double getGrippingForceNewtons(double gravityForceOnModuleNewtons) {
-
-        return gravityForceOnModuleNewtons * WHEELS_COEFFICIENT_OF_FRICTION;
-
-    }
-
-
-
-    public LinearVelocity maximumGroundSpeed() {
-
-        return MetersPerSecond.of(
-
-                driveMotorConfigs.freeSpinMechanismVelocity().in(RadiansPerSecond) * WHEEL_RADIUS.in(Meters));
-
-    }
-
-
-
-    public Force getTheoreticalPropellingForcePerModule(Mass robotMass, int modulesCount, Current statorCurrentLimit) {
-
-        final double
-
-                maxThrustNewtons =
-
-                        driveMotorConfigs.calculateTorque(statorCurrentLimit).in(NewtonMeters)
-
-                                / WHEEL_RADIUS.in(Meters),
-
-                maxGrippingNewtons = 9.8 * robotMass.in(Kilograms) / modulesCount * WHEELS_COEFFICIENT_OF_FRICTION;
-
-
-
-        return Newtons.of(Math.min(maxThrustNewtons, maxGrippingNewtons));
-
-    }
-
-
-
-    public LinearAcceleration maxAcceleration(Mass robotMass, int modulesCount, Current statorCurrentLimit) {
-
-        return getTheoreticalPropellingForcePerModule(robotMass, modulesCount, statorCurrentLimit)
-
-                .times(modulesCount)
-
-                .div(robotMass);
-
-    }
-
+  }
 }
-
